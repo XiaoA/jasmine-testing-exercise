bb57d4590e0a6001c10c03d68052afbebc58ef70
Andrew Buckingham <abuckingham99@gmail.com>
2020-11-26 19:58:57 -0800

    Feature (Error handling): It returns a "$0.00" with a null input

--- calculator/calculator-test.js
+++ calculator/calculator-test.js
@@ -18,5 +18,14 @@ it("should return a result with 2 decimal places", function() {
   expect(calculateMonthlyPayment(values)).toEqual('131.00');
 });
 
+it("should not return a result with missing data", function() {
+  const values = {
+    amount: null,
+    years: 8,
+    rate: 5.8
+  }
+  expect(calculateMonthlyPayment(values)).toEqual('0.00');
+})
+
 /// etc
 
