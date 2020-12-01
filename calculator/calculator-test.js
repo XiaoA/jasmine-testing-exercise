b9069225e742c5ecb7ff4a94f193d22a11b862cb
Andrew Buckingham <abuckingham99@gmail.com>
2020-12-01 14:27:28 -0800

    Implement (very basic) form validations

--- calculator/calculator-test.js
+++ calculator/calculator-test.js
@@ -1,31 +1,99 @@
+describe("Monthly calculation functions", function() {
+  it('should calculate the monthly rate correctly', function () {
+    const values = {
+      amount: 10000,
+      years: 8,
+      rate: 5.8
+    }
+    expect(calculateMonthlyPayment(values)).toEqual('130.44');
+  });
 
-it('should calculate the monthly rate correctly', function () {
-  const values = {
-    amount: 10000,
-    years: 8,
-    rate: 5.8
-  };
-  expect(calculateMonthlyPayment(values)).toEqual('130.44');
-});
+  it("should return a result with 2 decimal places", function() {
+    values = {
+      amount: 10043,
+      years: 8,
+      rate: 5.8
+    }
+    expect(calculateMonthlyPayment(values)).toEqual('131.00');
+  });
 
+  it("should update values on user input", function() {
+    values = {
+      amount: 5000,
+      years: 4,
+      rate: 3.2
+    }
 
-it("should return a result with 2 decimal places", function() {
-  const values = {
-    amount: 10043,
-    years: 8,
-    rate: 5.8
-  }
-  expect(calculateMonthlyPayment(values)).toEqual('131.00');
-});
-
-it("should not return a result with missing data", function() {
-  const values = {
-    amount: null,
-    years: 8,
-    rate: 5.8
-  }
-  expect(calculateMonthlyPayment(values)).toEqual('0.00');
+    function update(values) {
+      values = {
+        amount: 4000,
+        years: 3,
+        rate: 1.9
+      }
+    }
+    expect(calculateMonthlyPayment(values)).toEqual('111.11');
+  })
 })
 
-/// etc
+describe("simple form validations", function() {
+  it("should throw an error when 'amount' input is empty", function(){
+    const values = {
+      amount: null,
+      years: 8,
+      rate: 5.8
+    }
+    expect(calculateMonthlyPayment(values)).toEqual('0.00');
+    expect(function() {parser.parse(raw); } ).toThrowError();
+  })
+
+  it("should throw an error when 'years' input is empty", function(){
+    const values = {
+      amount: 3000,
+      years: null,
+      rate: 5.8
+    }
+    expect(calculateMonthlyPayment(values)).toEqual('Infinity');
+    expect(function() {parser.parse(raw); } ).toThrowError();
+  })
+
+  it("should throw an error when 'rate' input is empty", function(){
+    const values = {
+      amount: 3000,
+      years: 8,
+      rate: null
+    }
+    expect(calculateMonthlyPayment(values)).toEqual('NaN');
+    expect(function() {parser.parse(raw); } ).toThrowError();
+  })
+
+  it("should throw an error when 'rate' input is not a number", function(){
+    const values = {
+      amount: 3000,
+      years: 8,
+      rate: "hi"
+    }
+    expect(calculateMonthlyPayment(values)).toEqual('NaN');
+    expect(function() {parser.parse(raw); } ).toThrowError();
+  })
+
+  it("should throw an error when 'years' input is not a number", function(){
+    const values = {
+      amount: 3000,
+      years: "hi",
+      rate: 5.8
+    }
+    expect(calculateMonthlyPayment(values)).toEqual('NaN');
+    expect(function() {parser.parse(raw); } ).toThrowError();
+  })
+
+  it("should throw an error when 'amount' input is not a number", function(){
+    const values = {
+      amount: 3000,
+      years: "hi",
+      rate: 5.8
+    }
+    expect(calculateMonthlyPayment(values)).toEqual('NaN');
+    expect(function() {parser.parse(raw); } ).toThrowError();
+  })
+})
 
--- calculator/calculator.js
+++ calculator/calculator.js
@@ -10,35 +10,58 @@ window.addEventListener('DOMContentLoaded', function() {
 });
 
 function getCurrentUIValues() {
-  return {
+  const amountError = document.querySelector("#amount-error");
+  const yearError = document.querySelector("#year-error");
+  const rateError = document.querySelector("#rate-error");
+  
+  currentUIValuesObject = {
     amount: +(document.getElementById("loan-amount").value),
     years: +(document.getElementById("loan-years").value),
     rate: +(document.getElementById("loan-rate").value),
   }
+
+  let amount = document.querySelector("#loan-amount").value;
+  if (!(typeof amount === "number" || amount.length < 1)) {
+    amountError.innerText = ("Value must be a positive number.");
+    throw new TypeError("Value must be a positive number.");
+  } else { amount };
+  
+  let years = document.querySelector("#loan-years").value;
+  if (!(typeof years === "number" || years.length < 1)) {
+    yearError.innerText = ("Value must be a positive number.");
+    throw new TypeError("Value must be a positive number.");
+  } else { years };
+  
+  let rate = document.querySelector("#loan-rate").value;
+  if (!(typeof rate === "float" || rate.length < 1)) {
+    rateError.innerText = ("Value must be a positive number.");
+    throw new TypeError("Value must be a positive number.");
+  } else { rate };
+
+  return currentUIValuesObject;
 }
 
 // Get the inputs from the DOM.
 // Put some default values in the inputs
 // Call a function to calculate the current monthly payment
 function setupIntialValues() {
-  const values = { amount: 10000, years: 10, rate: 4.5 };
+  const values  = { amount: 10000, years: 10, rate: 4.5 };
   
   const amountUI = document.querySelector("#loan-amount");
   amountUI.value = values.amount;
 
-  const yearsUI = document.querySelector("loan-years");
+  const yearsUI = document.querySelector("#loan-years");
   yearsUI.value = values.years;
 
-  const rateUI = document.querySelector("loan-rate");
+  const rateUI = document.querySelector("#loan-rate");
   rateUI.value = values.rate;
-  update();
 }
 
 // Get the current values from the UI
 // Update the monthly payment
 function update() {
   const currentUIValues = getCurrentUIValues();
-  UpdateMonthly(calculateMonthlyPayment(currentUIValues));
+  updateMonthly(calculateMonthlyPayment(currentUIValues));
 }
 
 // Given an object of values (a value has amount, years and rate ),
@@ -59,4 +82,3 @@ function updateMonthly(monthly) {
   const monthlyUI = document.querySelector("#monthly-payment");
   monthlyUI.innerText = `${monthly}`
 }
-
