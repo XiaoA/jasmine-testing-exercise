50f82f9194b930675e0a7a4478e05510725cb3d9
Andrew Buckingham <abuckingham99@gmail.com>
2020-12-01 23:32:02 -0800

    Complete Tip Pool Step One: "Complete server.test.js"

--- tip-pool/servers.test.js
+++ tip-pool/servers.test.js
@@ -11,7 +11,46 @@ describe("Servers test (with setup and tear-down)", function() {
     expect(allServers['server' + serverId].serverName).toEqual('Alice');
   });
 
+  it("should allow more than one server", function() {
+    const allServers = {server1: {serverName: "Alice"}, server2: {serverName: "Bill"}}
+    submitServerInfo();
+    
+    expect(Object.keys(allServers).length).toEqual(2);
+    expect(allServers.server1.serverName).toBe('Alice');
+    expect(allServers.server2.serverName).toBe('Bill');
+  })
+
+  it("should split tip evenly among servers", function() {
+    const allServers = {server1: {serverName: "Alice"}, server2: {serverName: "Bill"}}
+    const allPayments = {
+      payment1:
+      {
+        billAmt: 100,
+        tipAmt: 20,
+        tipPercent: 20
+      }, 
+      payment2:
+      {
+        billAmt: 50,
+        tipAmt: 10,
+        tipPercent: 20
+      }
+    };
+    const tipAmt = parseInt(allPayments.payment1.tipAmt) + parseInt(allPayments.payment2.tipAmt)
+    const tipAverage = tipAmt / Object.keys(allServers).length;
+
+    submitServerInfo();
+
+    expect(Object.keys(allServers).length).toEqual(2);
+    expect(tipAmt).toEqual(30);
+    expect(tipAverage).toEqual(15);
+  });
+  
   afterEach(function() {
     // teardown logic
+    serverNameInput.value = '';
+    serverTbody.innerHTML = '';
+    allServers = {};
+    serverId = 0;
   });
 });
